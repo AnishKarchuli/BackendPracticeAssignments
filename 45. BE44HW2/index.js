e3b2707141c744c3cfb1fc5eaae00abfd79317d6
AnishKarchuli <aksk099@hotmail.com>
2024-12-12 15:25:34 +0530

    CORS policy updated

--- 45. BE44HW2/index.js
+++ 45. BE44HW2/index.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 require("dotenv").config();
 const { initializeDatabase } = require("./db/db.connect");
